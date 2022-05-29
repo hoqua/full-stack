53333fe0ff92155c4cc0801b73a96278ac272d7b
hoqua <dmitrii.zolotukhin@gmail.com>
2022-05-29 11:01:44 +0300

    first commit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,25 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import {  Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const globalPrefix = 'graphql';
   const port = process.env.PORT || 3333;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true }
+    })
+  )
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
